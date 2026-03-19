b676bcb8f26b31bcee475250cb2dd2883d92b162
@matias_serrano <matias.serrano@flockit.com.ar>
2026-03-19 10:43:31 -0300

    Angular 21

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection(),provideAnimationsAsync()]
 }).catch((err) => console.error(err));
